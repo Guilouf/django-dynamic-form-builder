767a310dc31213d72d2e4e752cbe8a7710818ec1
guigui <guillaume.lebreton@inra.fr>
2019-06-12 11:34:07 +0200

    Added Descriptor template model, which stores form templates

--- dynamic_form_builder/admin.py
+++ dynamic_form_builder/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+from dynamic_form_builder import models
+
+admin.site.register(models.DescriptorTemplate)
--- dynamic_form_builder/models.py
+++ dynamic_form_builder/models.py
@@ -1,1 +1,38 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+from django.conf import settings
+
+import yaml
+
+
+def choice_limit():
+    """Get the 'DYNAMIC_FORM_BUILDER_TARGETS' constant from the settings file to display
+    models from the targeted app"""
+    return {'app_label': getattr(settings, 'DYNAMIC_FORM_BUILDER_TARGET', None)}
+
+
+class DescriptorTemplate(models.Model):
+    """Model for building forms from YAML Charfield"""
+    slug = models.SlugField(max_length=200, unique=True, verbose_name=_("Identifier"))
+    type = models.ForeignKey(ContentType,
+                             limit_choices_to=choice_limit,
+                             on_delete=models.CASCADE, verbose_name=_("Type"))
+    yaml_descriptor = models.TextField(verbose_name=_("Key value template"),
+                                       help_text=_('Input as YAML'))
+
+    def __str__(self):
+        return self.slug
+
+    def clean(self):
+        """Check if it's valid YAML"""
+        try:
+            yaml.safe_load(self.yaml_descriptor)  # loads just basic tags
+        except Exception as e:
+            raise ValidationError(f'Not valid YAML: {e}')
+
+    @property
+    def descriptor_as_dict(self):
+        """Converts the YAML field into dictionary"""
+        return yaml.safe_load(self.yaml_descriptor)
